0131736ce9f90b4e22e7f71ff28093dfcb904d08
meimeihuynh <ngohuy02@innlandetfylke.no>
2026-05-19 12:18:53 +0200

    prep

---
+++ INGEN MAL/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import { react } from "react";
+import Header from "../components/header";
+
+function Home() {
+    return (
+        <div>
+            <Header />
+        </div>
+    );
+}
+
+export default Home;
